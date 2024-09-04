3d046d646abd49a885690fdb305106afcb8dd83b
DevRajyguru <devrajyguru07>
2024-09-04 22:21:51 +0530

    initial commit

---
+++ script5.php
@@ -0,0 +1,9 @@
+<?php
+
+$firstname = "Dev"; 
+$lastname = "Rajyguru";   
+
+
+echo "First Name: " . $firstname . "<br>";
+echo "Last Name: " . $lastname;
+?>
---
+++ script8.php
@@ -0,0 +1,13 @@
+<?php
+$name="Dev";
+$age=21;
+$city="Rajkot";
+
+
+echo "Name = $name <br>";
+echo "Age = $age <br>";
+echo "City = $city";
+
+
+
+?>   
